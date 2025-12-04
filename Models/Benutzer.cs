a0ebedcbd87988b4d7679ddcb30bcea4784aeef7
ElJefe <dimitri.bredhauer@training.lug-ag.de>
2025-12-04 10:08:49 +0100

    Ordnerstruktur angelegt

---
+++ Models/Benutzer.cs
@@ -0,0 +1,7 @@
+﻿namespace Zoo_Manager {
+	internal record Benutzer {
+		public string Benutzername { get; set; } = "";
+		public string Passwort { get; set; } = string.Empty;
+		public Rolle Rolle { get; set; } = Rolle.Gast;
+	}
+}
