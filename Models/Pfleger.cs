18303dc5fff7ee4ca25e33d702dfad6b7a9c5cfb
ElJefe <dimitri.bredhauer@training.lug-ag.de>
2025-12-04 10:56:08 +0100

    Klasse Pfleger erstellt

---
+++ Models/Pfleger.cs
@@ -0,0 +1,12 @@
+﻿namespace Zoo_Manager.Models {
+	internal class Pfleger {
+
+		public int Id { get; set; }
+		public string Name { get; set; } = string.Empty;
+		public string Einsatzort { get; set; } = string.Empty;  //z.B. Reptilienhaus
+
+		public override string ToString() {
+			return $"ID: {Id}, Name: {Name}, Einsatzort: {Einsatzort}";
+		}
+	}
+}
