7cc8fd817d2bd93b078fb56fa810e91bb2088f53
ElJefe <dimitri.bredhauer@training.lug-ag.de>
2025-12-08 23:02:09 +0100

    hinzufügen von verschiedenen Gehegetypen mit Vererbung

---
+++ Models/Afrikasavanne.cs
@@ -0,0 +1,9 @@
+﻿namespace Zoo_Manager.Models {
+	internal class Afrikasavanne : Gehege {
+		public bool HatWasserstelle { get; set; }
+
+		public override string ToString() {
+			return base.ToString() + $", Wasserstelle: {(HatWasserstelle ? "Ja" : "Nein")}";
+		}
+	}
+}
---
+++ Models/Raubtierhaus.cs
@@ -0,0 +1,9 @@
+﻿namespace Zoo_Manager.Models {
+	internal class Raubtierhaus : Gehege {
+		public bool HatZaun { get; set; }
+
+		public override string ToString() {
+			return base.ToString() + $", Zaun: {(HatZaun ? "Ja" : "Nein")}";
+		}
+	}
+}
