3ce2fc5946485c6034590be0cd9df1ca6b1b3306
ElJefe <dimitri.bredhauer@training.lug-ag.de>
2025-12-08 23:10:14 +0100

    Gehege-Suche intergriert

--- Services/GehegeService.cs
+++ Services/GehegeService.cs
@@ -3,17 +3,14 @@ using Zoo_Manager.Repository;
 
 namespace Zoo_Manager.Services {
 	internal class GehegeService : IService {
-
 		public readonly string _filePath = "gehege.json";
 		private List<Gehege> _gehege;
 
 		public GehegeService() {
-
 			_gehege = JsonRepository.LadeDaten<Gehege>(_filePath);
 		}
 
 		public List<Gehege> GetAll() => _gehege;
-
 		public void Hinzufuegen(Gehege gehege) {
 			gehege.Id = _gehege.Any() ? _gehege.Max(g => g.Id) + 1 : 1;
 			_gehege.Add(gehege);
@@ -30,15 +27,20 @@ namespace Zoo_Manager.Services {
 			return false;
 		}
 
-		public void Update(Gehege updatedGehege) {
-
+		public bool Update(Gehege updatedGehege) {
 			var gehege = _gehege.FirstOrDefault(g => g.Id == updatedGehege.Id);
 			if (gehege != null) {
-				gehege.Name = updatedGehege.Name;
+				gehege.Name = gehege.Name;
 				gehege.Lage = updatedGehege.Lage;
 				gehege.AnzahlTiere = updatedGehege.AnzahlTiere;
 				JsonRepository.SpeichereDaten(_gehege, _filePath);
+				return true;
 			}
+			return false;
+		}
+
+		public Gehege? Suche(int gehegeId) {
+			return _gehege.FirstOrDefault(g => g.Id == gehegeId);
 		}
 	}
 }
