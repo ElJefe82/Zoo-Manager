9d1321ec2846d5579306eec6a3db480334334d28
ElJefe <dimitri.bredhauer@training.lug-ag.de>
2025-12-09 13:55:36 +0100

    Eigenschaft umbenannt

--- Models/Tier.cs
+++ Models/Tier.cs
@@ -4,10 +4,10 @@
 		public string Name { get; set; } = string.Empty;
 		public string Art { get; set; } = string.Empty;
 		public int Alter { get; set; }
-		public int GehegeId { get; set; } // Verweis auf Gehege
+		public string GehegeName { get; set; } = string.Empty; // Verweis auf Gehege
 
 		public override string ToString() {
-			return $"{Id}) Name: {Name}, Art: {Art}, Alter: {Alter} Jahre, Gehege: {GehegeId}";
+			return $"{Id}) Name: {Name}, Art: {Art}, Alter: {Alter} Jahre, Gehege: {GehegeName}";
 		}
 	}
 }
